bd75c856262585ea4e214f622084fe6ba5b387b9
Enes Çakmak <cakmakkeness@gmail.com>
2023-07-25 22:50:08 +0300

    Create more cars and improved movement

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,35 @@
+import turtle
+from random import choice, randint
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+is_game_on = True
 
 
 class CarManager:
-    pass
+    def __init__(self, level):
+        self.car_list = []
+        self.create_cars()
+        self.movement(level)
+
+    def create_cars(self):
+        turtle.tracer(0)
+        for _ in range(20):
+            car = Turtle("square")
+            car.color(choice(COLORS))
+            car.shapesize(1, 3, 1)
+            car.penup()
+            car.goto(randint(-280, 280), randint(-220, 260))
+            car.setheading(180)
+            self.car_list.append(car)
+
+    def movement(self, level):
+        for car in self.car_list:
+            car.forward(STARTING_MOVE_DISTANCE + level * MOVE_INCREMENT)
+            if car.xcor() <= -310:
+                car.hideturtle()
+                car.goto(320, randint(-260, 240))
+                car.color(choice(COLORS))
+                car.showturtle()
