8d2b398ed4e6638f667d40d49048dd426f08aa23
Enes Çakmak <cakmakkeness@gmail.com>
2023-07-25 23:19:03 +0300

    Added game over when crash

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import time
+import turtle
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-level = 1
+level = 0
+game_is_on = True
 
 player = Player()
 screen = Screen()
 cars = CarManager(level)
+scoreboard = Scoreboard(level)
 
 screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.listen()
-screen.onkey(player.move, "Up")
-screen.onkey(player.move, "w")
+screen.onkeypress(player.move, "Up")
+screen.onkeypress(player.move, "w")
 
-game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    cars.movement(level)
+
+    for car in cars.car_list:
+        if player.distance(car) < 25:
+            scoreboard.game_over()
+            game_is_on = False
 
 screen.exitonclick()
