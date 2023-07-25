b4d8b7449ca3ee72ac942081aadbd666dadcc9ee
Enes Çakmak <cakmakkeness@gmail.com>
2023-07-25 21:55:07 +0300

    Created player, screen and cars. Added movements

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+level = 1
+
+player = Player()
 screen = Screen()
+cars = CarManager(level)
+
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+screen.onkey(player.move, "Up")
+screen.onkey(player.move, "w")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
